31486cf1adccdb38dc7b5156e9f38002aac26a75
schech1 <76683319+schech1@users.noreply.github.com>
2023-03-06 10:36:27 +0100

    all

---
+++ shared/build/generated/source/buildConfig/release/com/example/testlibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.testlibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.testlibrary";
+  public static final String BUILD_TYPE = "release";
+}
