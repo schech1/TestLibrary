deb9f88cef07be92d70a28666bec76ef63946b20
schech1 <76683319+schech1@users.noreply.github.com>
2023-03-06 10:58:07 +0100

    r

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,5 @@
 val frameworkVersion = "1.0.0"
-val frameworkName = "ExampleProject"
+val frameworkName = "shared"
 
 plugins {
     kotlin("multiplatform")
@@ -19,7 +19,7 @@ kotlin {
     }
     iosX64()
     iosArm64()
-  //  iosSimulatorArm64()
+    iosSimulatorArm64()
 
     cocoapods {
         summary = "This is the shared EL lib"
@@ -42,21 +42,21 @@ kotlin {
         val androidUnitTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
-    //    val iosSimulatorArm64Main by getting
+        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-        //    iosSimulatorArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-      //  val iosSimulatorArm64Test by getting
+        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-       //     iosSimulatorArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
